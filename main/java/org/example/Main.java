5b9c2714b8a39cffd6722fd2be4af6da361c57fd
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2024-11-26 19:39:29 +0000

    task 1 - create a menu done

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -1,12 +1,43 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("hello");
-        System.out.println("test");
+        menu(scanner);
         scanner.close();
+    }
+
+    public static void menu(Scanner scanner) {
+        int choice = 0;
+        String filePath;
+
+        do {
+            System.out.println("\nChoose an option by number: ");
+            System.out.println("1. Encrypt a file");
+            System.out.println("2. Decrypt a file");
+            System.out.println("3. Exit");
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (choice == 1) {
+                    System.out.println("\nEnter file path/name: ");
+                    filePath = scanner.nextLine();
+                    System.out.println("encrypt");
+                } else if (choice == 2) {
+                    System.out.println("Decrypt!");
+                } else if (choice == 3) {
+                    System.out.println("Bye!");
+                } else {
+                    System.out.println("\nInvalid choice. Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nInvalid input. Please enter a number.");
+                scanner.nextLine();
+            }
+        } while (choice != 3);
     }
 }
